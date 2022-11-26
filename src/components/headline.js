00e0ea158c447466c2769346399e1dab5f453242
johndel siriban <MY_NAME@example.com>
2022-11-26 14:46:24 +0800

    CHANGES

--- src/components/headline.js
+++ src/components/headline.js
@@ -4,15 +4,14 @@ import "./headstyle.css";
 const HeadLine =(props)=>{
     return(
         <div className="Product">
-            return(
+          
                 <div className="head">
                     <div className="heading">
                        <p id="coffee-intro">{props.heading}</p>
                        <p id="quotes">{props.intro}</p>
                     </div>
                 </div>
-            )
-        
+            
         </div>
     )
 }
