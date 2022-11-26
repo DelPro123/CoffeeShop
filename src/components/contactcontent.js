d9197f3e1e5a4fe78472c8069752f03a237432de
johndel siriban <MY_NAME@example.com>
2022-11-26 15:16:02 +0800

    contact changed

--- src/components/contactcontent.js
+++ src/components/contactcontent.js
@@ -4,15 +4,12 @@ import "../components/contactcontent.css"
 const ContactContent=()=>{
     return(
         <div className="contact-content">
-            <div className="contact-flex">
             <div class="mapouter">
                 <div class="gmap_canvas">
                     <iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=500&amp;height=500&amp;hl=en&amp;q=Caffeine Ave., Pilapil Dental Clinic, Tanay, Rizal, Philippines&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" title="myFrame">
                     </iframe>
                 </div>
             </div>
-            </div>
-            <div className="contact-flex">
                 <div className="form">
                     <form>
                         <label>Your Name</label>
@@ -26,7 +23,6 @@ const ContactContent=()=>{
                         <button to="/contact" className="Product-btn">Submit</button>
                     </form>
                 </div>
-            </div>
         </div>
     
     )
