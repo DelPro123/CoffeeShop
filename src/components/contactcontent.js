cc1ddf630b642c2ae57bf37114e799b5559f6e20
johndel siriban <MY_NAME@example.com>
2022-11-26 15:18:31 +0800

    changed

--- src/components/contactcontent.js
+++ src/components/contactcontent.js
@@ -12,12 +12,12 @@ const ContactContent=()=>{
             </div>
                 <div className="form">
                     <form>
-                        <label>Your Name</label>
-                        <input type="text"></input>
+                        <label>Your name</label>
+                        <input type="text" placeholder="Enter your name"></input>
                         <label>Email</label>
                         <input type="email" placeholder="Enter your email"></input>
                         <label>Subject</label>
-                        <input type="text"></input>
+                        <input type="text" placeholder="Enter your subject"></input>
                         <label>Messages</label>
                         <textarea rows="6" placeholder="Type your messages"></textarea>
                         <button to="/contact" className="Product-btn">Submit</button>
